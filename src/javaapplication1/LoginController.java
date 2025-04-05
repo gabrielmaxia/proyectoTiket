be6b752ba7d9ae7d9b87b7c4f4c553e8100eba89
Gabriel Maxia <ag0451507@gmail.com>
2025-04-05 07:36:39 -0600

    Subida a un repositorio diferente

---
+++ src/javaapplication1/LoginController.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javaapplication1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+
+public class LoginController{
+@FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private void paantallaPrincipal() {
+        // Lógica de inicio de sesión (por ahora vacía)
+        errorLabel.setText("Funcionalidad no implementada aún.");
+    
+}
+}
